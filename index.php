d065a11f6fbecb53e509f5a542690b8d57ffb297
showkotreza <57698416+showkotreza@users.noreply.github.com>
2020-02-10 11:49:19 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LogIn Form</title>
+</head>
+<body>
+<form action="task.php" method="post">
+<fieldset>
+	<legend><b>Login Form</b></legend>
+  First name:
+  <input type="text" name="firstname"><br>
+  Last name:
+  <input type="text" name="lastname"><br>
+   Email:
+  <input type="text" name="email"><br><br>
+   <input type="submit" value="Submit">
+</fieldset>
+</form>
+</body>
+</html>
---
+++ task.php
@@ -0,0 +1,9 @@
+<?php
+echo "My name is showkot";
+$a=$_POST["email"];
+echo "<br>";
+echo $_POST["name"];
+var_dump($_POST);
+
+echo $a;
+?>
